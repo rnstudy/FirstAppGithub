25b49958c4687e1f49a7b3c9176627e4d239ccc7
wurh <ro87630872@126.com>
2018-06-09 16:11:04 +0800

    【UPD】更新项目结构
    更新项目结构

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@
 import React, { Component } from 'react';
 import TabNavigator from 'react-native-tab-navigator';
 import {Navigator} from 'react-native-deprecated-custom-components';
-import Boy from './src/boy'
+import Boy from './boy'
 import {
   Platform,
   StyleSheet,
