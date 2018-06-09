7a368e15c039d7fb16cfeec4f23d94016881139f
wurh <ro87630872@126.com>
2018-06-09 12:03:23 +0800

    【UPD】第一节完成基础底部导航开发
    第一节完成基础底部导航开发

--- App.js
+++ App.js
@@ -5,9 +5,11 @@
  */
 
 import React, { Component } from 'react';
+import TabNavigator from 'react-native-tab-navigator';
 import {
   Platform,
   StyleSheet,
+  Image,
   Text,
   View
 } from 'react-native';
@@ -20,19 +22,59 @@ const instructions = Platform.select({
 });
 
 type Props = {};
+
 export default class App extends Component<Props> {
+  constructor(props){
+    super(props);
+    this.state = {
+      selectedTab:'home',
+    }
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!  
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+         <TabNavigator>
+            <TabNavigator.Item
+              selected={this.state.selectedTab === 'tb_popular'}
+              selectedTitleStyle={{color:'red'}}
+              title="最热"
+              renderIcon={() => <Image style={styles.imgae} source={require('./res/img/cart.png')} />}
+              renderSelectedIcon={() => <Image style={[styles.imgae,{tintColor:'red'}]} source={require('./res/img/cart.png')} />}
+              badgeText="2"
+              onPress={() => this.setState({ selectedTab: 'tb_popular' })}>
+              <View style={styles.page1}></View>
+            </TabNavigator.Item>
+            <TabNavigator.Item
+              selected={this.state.selectedTab === 'tb_trending'}
+               selectedTitleStyle={{color:'red'}}
+              title="趋势"
+              renderIcon={() => <Image style={styles.imgae} source={require('./res/img/store.png')} />}
+              renderSelectedIcon={() => <Image style={[styles.imgae,{tintColor:'red'}]}  source={require('./res/img/store.png')} />}
+              onPress={() => this.setState({ selectedTab: 'tb_trending' })}>
+             <View style={styles.page2}></View>
+              </TabNavigator.Item>
+              <TabNavigator.Item
+                  selected={this.state.selectedTab === 'tb_favor'}
+                  selectedTitleStyle={{color:'red'}}
+                  title="收藏"
+                  renderIcon={() => <Image style={styles.imgae} source={require('./res/img/sale.png')} />}
+                  renderSelectedIcon={() => <Image style={[styles.imgae,{tintColor:'red'}]} source={require('./res/img/sale.png')} />}
+                  badgeText="1"
+                  onPress={() => this.setState({ selectedTab: 'tb_favor' })}>
+                  <View style={styles.page1}></View>
+            </TabNavigator.Item>
+            <TabNavigator.Item
+                  selected={this.state.selectedTab === 'tb_my'}
+                   selectedTitleStyle={{color:'red'}}
+                  title="我的"
+                  renderIcon={() => <Image style={styles.imgae} source={require('./res/img/person.png')} />}
+                  renderSelectedIcon={() => <Image style={[styles.imgae,{tintColor:'red'}]}  source={require('./res/img/person.png')} />}
+                  onPress={() => this.setState({ selectedTab: 'tb_my' })}>
+                 <View style={styles.page2}></View>
+              </TabNavigator.Item>
+        </TabNavigator>
       </View>
     );
   }
@@ -41,18 +83,18 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  page1:{
+    flex:1,
+    backgroundColor: 'red'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  page2:{
+    flex:1,
+    backgroundColor: 'yellow'
   },
+  image:{
+    width:22,
+    height:22,
+  }
 });
