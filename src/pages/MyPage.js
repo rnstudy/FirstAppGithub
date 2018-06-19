cb65d2518f2af8b8fac4a7ba8fb29ec85c25680c
wurh <ro87630872@126.com>
2018-06-19 20:32:41 +0800

    【ADD】我的页面列表页A
    我的页面列表页A

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -4,12 +4,17 @@ import {
     StyleSheet,
     Image,
     Text,
-    View
+    View,
+    ScrollView,
+    TouchableHighlight
 } from 'react-native';
 import NavigationBar from './../component/NavigationBar'
 import CustomKeyPage from './CustomKeyPage'
 import SortKeyPage from './SortKeyPage'
 import LanguageDao,{FLAG_LANGUAGE} from '../expand/dao/LanguageDao'
+import TrendingCell from "../component/TrendingCell";
+import {MORE_MENU} from '../component/MoreMenu'
+import GlobalStyles from '../../res/styles/GlobalStyles'
 
 
 export default class MyPage extends Component {
@@ -18,80 +23,41 @@ export default class MyPage extends Component {
         this.state = {
         };
     }
+
+    onClick(tab){
+
+    }
+
     render(){
+        var navigationBar = <NavigationBar
+            title={'我的'}
+            style={{backgroundColor:'#2196f3'}}
+        />
+
+
         return (
             <View style={styles.container}>
-                <NavigationBar
-                    title={'我的'}
-                    style={{backgroundColor:'#2196f3'}}
-                />
-                <Text
-                    style={styles.tips}
-                    onPress={()=>{
-                        this.props.navigator.push({
-                            component:CustomKeyPage,
-                            params:{
-                                ...this.props,
-                                isRemoveKey:false,
-                                flag:FLAG_LANGUAGE.flag_key
-                            },
-
-                        })
-                    }}
-                >自定义标签</Text>
-                <Text
-                    style={styles.tips}
-                    onPress={()=>{
-                        this.props.navigator.push({
-                            component:CustomKeyPage,
-                            params:{
-                                ...this.props,
-                                isRemoveKey:false,
-                                flag:FLAG_LANGUAGE.flag_language
-                            },
-
-                        })
-                    }}
-                >自定义语言</Text>
-                <Text
-                    style={styles.tips}
-                    onPress={()=>{
-                        this.props.navigator.push({
-                            component:SortKeyPage,
-                            params:{
-                                ...this.props,
-                                flag:FLAG_LANGUAGE.flag_key
-                            },
-
-                        })
-                    }}
-                >标签排序</Text>
-                <Text
-                    style={styles.tips}
-                    onPress={()=>{
-                        this.props.navigator.push({
-                            component:SortKeyPage,
-                            params:{
-                                ...this.props,
-                                flag:FLAG_LANGUAGE.flag_language
-                            },
-
-                        })
-                    }}
-                >语言排序</Text>
-                <Text
-                    style={styles.tips}
-                    onPress={()=>{
-                        this.props.navigator.push({
-                            component:CustomKeyPage,
-                            params:{
-                                ...this.props,
-                                isRemoveKey:true,
-                                flag:FLAG_LANGUAGE.flag_key
-                            }
-                        })
-                    }}
-                >标签移除</Text>
+                {navigationBar}
+                <ScrollView>
+                    <TouchableHighlight
+                        onPress={()=>this.onClick(MORE_MENU.About)}
+                    >
+                        <View style={styles.item}>
+                            <View style={{flexDirection:'row',alignItems:'center'}}>
+                                <Image
+                                    source={require('../../res/img/person.png')}
+                                    style={[{width:40,height:40,marginRight:10},{tintColor:'#2196f3'}]}
+                                />
+                                <Text>GitHub Popular</Text>
+                            </View>
+                            <Image
+                                source={require('../../res/img/person.png')}
+                                style={[{width:22,height:22,marginRight:10},{tintColor:'#2196f3'}]}
+                            />
+                        </View>
+                    </TouchableHighlight>
+                    <View style={GlobalStyles.line}/>
+                </ScrollView>
             </View>
         )
     }
@@ -105,5 +71,13 @@ const styles = StyleSheet.create({
     },
     tips:{
         fontSize:20,
+    },
+    item:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center',
+        padding:10,
+        height:60
+
     }
 })
